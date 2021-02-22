18a72692f9f86019ab1597feec74898867a7a878
Wessel van der Linden <wessel.vanderlinden@infosupport.com>
2021-02-22 10:00:07 +0000

    added privacy filter

--- python/app.py
+++ python/app.py
@@ -2,7 +2,10 @@ import os
 import time
 import shutil
 import csv
-
+import pandas as pd
+import json
+import io
+import jsonpickle
 from flask import Flask, request, session, redirect, render_template
 from dotenv import load_dotenv
 import requests
@@ -43,16 +46,118 @@ def callback():
 
 @app.route('/filter')
 def filter():
-    print('hello')
+    #Get CSV File from ps2meniet for the latest data
     response = requests.request('GET',  "https://psd2meniet.nl/wp-content/uploads/2020/12/PSD2meniet-register-rekeningnummers-v20201204.csv")
-    decoded_content = response.content.decode('utf-16')
+    #decode unicode content in variable
+    decoded_content = response.text
+    
+    #import CSV file into panda's dataframe
+    AccountsToFiltersdf = pd.read_csv(io.StringIO(decoded_content), sep=';')
 
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    for row in my_list:
-        print(row)
+    #Import Json from file
+    jsonfile = open('./input/input.json')
+    jsonstring = jsonfile.read()
+    #Get JSON input from post request
+    jsonstring = request.data
+    #Get CSV File from ps2meniet for the latest data
+    response = requests.request('GET',  "https://psd2meniet.nl/wp-content/uploads/2020/12/PSD2meniet-register-rekeningnummers-v20201204.csv")
+    #decode unicode content in variable
+    decoded_content = response.text
+    
+    #import CSV file into panda's dataframe
+    AccountsToFiltersdf = pd.read_csv(io.StringIO(decoded_content), sep=';')
 
-    return render_template('home.html', accounts=response.content)
+    #Deserialize Json into Python Object
+    jsonObject = jsonpickle.decode(jsonstring)
+    #Get booked transactions
+    bookedTransactions = ((jsonObject.get('transactions')).get('booked'))
+    #Convert booked transactions list to panda's dataframe
+    df = pd.DataFrame(bookedTransactions)
+    #Convert Dictionary of DebtorAccount to Panda's series
+    series1 = df.debtorAccount.apply(pd.Series)
+    #Remove DebtorAccount Dictionary column and replace with the columns from the Panda's Series
+    df = pd.concat([df.drop(['debtorAccount'], axis=1), df['debtorAccount'].apply(pd.Series)], axis=1)
+    #Filter iban's based on the AccountsToFiltersdf
+    filter = df['iban'].isin(AccountsToFiltersdf['IBANzonderBIC'])
+    #Get index of ibans which are found
+    indexArray = df[filter].index
+    #Sort by descending indexes to delete list indexes in the correct sequence
+    indexArray = indexArray.sort_values(ascending=False)
+    #loop through the index of found values to delete list indices
+    for index in indexArray:
+        del(bookedTransactions[index])
+
+    #Get Pendingtransactions
+    pendingTransactions = ((jsonObject.get('transactions')).get('pending'))
+    #Convert Pending transactions to panda's dataframe
+    df = pd.DataFrame(pendingTransactions)
+    #Convert Dictionary of CreditorAccount to Panda's series
+    series1 = df.creditorAccount.apply(pd.Series)
+    #Remove CreditorAccount Dictionary column and replace with the columns from the Panda's Series
+    df = pd.concat([df.drop(['creditorAccount'], axis=1), df['creditorAccount'].apply(pd.Series)], axis=1)
+    #Filter iban's based on the AccountsToFiltersdf
+    filter = df['iban'].isin(AccountsToFiltersdf['IBANzonderBIC'])
+    #Get index of ibans which are found
+    indexArray = df[filter].index
+    #Sort by descending indexes to delete list indexes in the correct sequence
+    indexArray = indexArray.sort_values(ascending=False)
+
+    #Serialize Python Object in JSON
+    output = jsonpickle.encode(jsonObject)
+    #Return json output
+    return render_template('home.html', accounts=output)
+
+@app.route('/apply-filter',methods=['POST'])
+def applyfilter():
+    #Get JSON input from post request
+    jsonstring = request.data
+    #Get CSV File from ps2meniet for the latest data
+    response = requests.request('GET',  "https://psd2meniet.nl/wp-content/uploads/2020/12/PSD2meniet-register-rekeningnummers-v20201204.csv")
+    #decode unicode content in variable
+    decoded_content = response.text
+    
+    #import CSV file into panda's dataframe
+    AccountsToFiltersdf = pd.read_csv(io.StringIO(decoded_content), sep=';')
+
+    #Deserialize Json into Python Object
+    jsonObject = jsonpickle.decode(jsonstring)
+    #Get booked transactions
+    bookedTransactions = ((jsonObject.get('transactions')).get('booked'))
+    #Convert booked transactions list to panda's dataframe
+    df = pd.DataFrame(bookedTransactions)
+    #Convert Dictionary of DebtorAccount to Panda's series
+    series1 = df.debtorAccount.apply(pd.Series)
+    #Remove DebtorAccount Dictionary column and replace with the columns from the Panda's Series
+    df = pd.concat([df.drop(['debtorAccount'], axis=1), df['debtorAccount'].apply(pd.Series)], axis=1)
+    #Filter iban's based on the AccountsToFiltersdf
+    filter = df['iban'].isin(AccountsToFiltersdf['IBANzonderBIC'])
+    #Get index of ibans which are found
+    indexArray = df[filter].index
+    #Sort by descending indexes to delete list indexes in the correct sequence
+    indexArray = indexArray.sort_values(ascending=False)
+    #loop through the index of found values to delete list indices
+    for index in indexArray:
+        del(bookedTransactions[index])
+
+    #Get Pendingtransactions
+    pendingTransactions = ((jsonObject.get('transactions')).get('pending'))
+    #Convert Pending transactions to panda's dataframe
+    df = pd.DataFrame(pendingTransactions)
+    #Convert Dictionary of CreditorAccount to Panda's series
+    series1 = df.creditorAccount.apply(pd.Series)
+    #Remove CreditorAccount Dictionary column and replace with the columns from the Panda's Series
+    df = pd.concat([df.drop(['creditorAccount'], axis=1), df['creditorAccount'].apply(pd.Series)], axis=1)
+    #Filter iban's based on the AccountsToFiltersdf
+    filter = df['iban'].isin(AccountsToFiltersdf['IBANzonderBIC'])
+    #Get index of ibans which are found
+    indexArray = df[filter].index
+    #Sort by descending indexes to delete list indexes in the correct sequence
+    indexArray = indexArray.sort_values(ascending=False)
+
+    #Serialize Python Object in JSON
+    output = jsonpickle.encode(jsonObject)
+    #Return json output
+    return json.dumps(output)
 
 
 @app.route('/accounts')
